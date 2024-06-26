653961a5248032af9b5f44e42eaa9adb260ec2fb
gitco-a <aniket.vadar.official@gmail.com>
2024-06-26 18:54:18 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -2,7 +2,5 @@
 export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
-    dbCredentials: {
-      url: 'postgresql://ai_mock_owner:xnw7XmdKySG5@ep-cold-darkness-a1msgunh.ap-southeast-1.aws.neon.tech/ai_mock?sslmode=require',
-    }
+ 
   };
